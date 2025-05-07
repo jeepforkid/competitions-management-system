98dfdd6fa11b713ed821200da7fa74ddabd893c4
jeepforkid <jeepforkid@gmail.com>
2025-05-07 11:31:54 +0400

    Add app.js 1746603113993

---
+++ app.js
@@ -0,0 +1,92 @@
+// استيراد المكتبات المطلوبة
+const express = require('express');
+const session = require('express-session');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+const path = require('path');
+const config = require('./config');
+
+// استيراد النماذج
+const db = require('./models');
+
+// إنشاء تطبيق Express
+const app = express();
+
+// إعداد محرك العرض EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// الوسائط (Middleware)
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+// إعداد الجلسات
+app.use(session({
+    secret: config.sessionSecret,
+    resave: false,
+    saveUninitialized: false
+}));
+
+// إعداد الرسائل الفلاشية
+app.use(flash());
+
+// متغيرات عامة للقوالب
+app.use((req, res, next) => {
+    res.locals.user = req.session.user;
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
+
+// التحقق من المصادقة
+const ensureAuthenticated = (req, res, next) => {
+    if (req.session.user) {
+        return next();
+    }
+    req.flash('error_msg', 'يرجى تسجيل الدخول للوصول إلى هذه الصفحة');
+    res.redirect('/auth/login');
+};
+
+// استيراد المسارات
+const authRoutes = require('./routes/auth');
+const contestantRoutes = require('./routes/contestants');
+const supervisorRoutes = require('./routes/supervisors');
+const competitionRoutes = require('./routes/competitions');
+const scoreRoutes = require('./routes/scores');
+
+// تسجيل المسارات
+app.use('/auth', authRoutes);
+app.use('/contestants', ensureAuthenticated, contestantRoutes);
+app.use('/supervisors', ensureAuthenticated, supervisorRoutes);
+app.use('/competitions', ensureAuthenticated, competitionRoutes);
+app.use('/scores', ensureAuthenticated, scoreRoutes);
+
+// توجيه الصفحة الرئيسية إلى صفحة الدرجات
+app.get('/', (req, res) => {
+    res.redirect('/scores');
+});
+
+// معالجة الأخطاء 404
+app.use((req, res) => {
+    res.status(404).render('errors/404', {
+        title: 'الصفحة غير موجودة',
+        message: 'عذراً، الصفحة التي تبحث عنها غير موجودة'
+    });
+});
+
+// معالجة الأخطاء العامة
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).render('errors/500', {
+        title: 'خطأ في الخادم',
+        message: 'عذراً، حدث خطأ في الخادم'
+    });
+});
+
+// تشغيل الخادم
+const PORT = config.port;
+app.listen(PORT, () => {
+    console.log(`تم تشغيل الخادم على المنفذ ${PORT}`);
+});
